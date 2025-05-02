c9e36f737ebbec51a3e18c9b0d84587155957693
omayma-gam <gamranioumayma@gmail.com>
2025-05-02 16:53:54 +0100

    quelque modification

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,14 @@ import {RecipeListComponent} from './recipe-list/recipe-list.component';
 //import {RecipeDetailComponent} from './recipe-detail/recipe-detail.component';
 import {FormRecipeComponent} from './form-recipe/form-recipe.component';
 import {RatingSystemComponent} from './rating-system/rating-system.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 
 const routes: Routes = [
   {path : "login", component : LoginComponent},
   {path : "home" , component : HomeComponent},
   {path : "form" , component : FormRecipeComponent},
   {path : "recipe-list", component : RecipeListComponent},
-  {path : "recipe-detail/:id",loadChildren : () => import('./recipe-detail/recipe-detail.component').then(E =>E.RecipeDetailComponent)},
+  {path : "recipe-detail/:id", component : RecipeDetailComponent},
   {path : "rating", component : RatingSystemComponent},
 ];
 
